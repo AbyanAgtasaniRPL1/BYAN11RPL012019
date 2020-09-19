9d742303756b685ec147ba03dd401be0c0ec4a7b
AbyanAgtasaniRPL1 <byan.10rpl1.2019@gmail.com>
2020-09-19 21:03:02 +0700

    add realm

--- app/src/main/java/com/example/byan11rpl012019/ListData.java
+++ app/src/main/java/com/example/byan11rpl012019/ListData.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Adapter;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
@@ -26,7 +28,6 @@ public class ListData extends AppCompatActivity {
     private dataadapter adapter;
     private ArrayList<model> DataArrayList; //kit add kan ke adapter
     private ImageView tambah_data;
-    private Object Adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,6 @@ public class ListData extends AppCompatActivity {
         //addData();
         addDataOnline();
     }
-
     void addData() {
         //offline, isi data offline dulu
         DataArrayList = new ArrayList<>();
@@ -50,7 +50,7 @@ public class ListData extends AppCompatActivity {
         DataArrayList.add(data1);
 
 
-        adapter = new dataadapter (DataArrayList, new dataadapter.Callback() {
+        adapter = new dataadapter(DataArrayList, new dataadapter.Callback() {
             @Override
             public void onClick(int position) {
 
@@ -60,7 +60,6 @@ public class ListData extends AppCompatActivity {
             public void test() {
 
             }
-
         });
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
         recyclerView.setLayoutManager(layoutManager);
@@ -70,8 +69,8 @@ public class ListData extends AppCompatActivity {
 
 
     }
-    //void addData()
-    void addDataOnline() {
+
+    void addDataOnline(){
         AndroidNetworking.get("https://api.themoviedb.org/3/movie/now_playing?api_key=6ac7a042ac3b7599a689eb943fa0b6d0&language=en-US")
                 .setTag("test")
                 .setPriority(Priority.LOW)
@@ -79,17 +78,19 @@ public class ListData extends AppCompatActivity {
                 .getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        // do anything with response
                         Log.d("hasiljson", "onResponse: " + response.toString());
-
+                        //jika sudah berhasil debugm lanjutkan code dibawah ini
                         DataArrayList = new ArrayList<>();
                         model modelku;
                         try {
                             Log.d("hasiljson", "onResponse: " + response.toString());
                             JSONArray jsonArray = response.getJSONArray("results");
                             Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
-                            for (int i = 0; i < jsonArray.length(); i++){
+                            for (int i = 0; i < jsonArray.length(); i++) {
                                 modelku = new model();
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                modelku.setId(jsonObject.getInt("id"));
                                 modelku.setOriginal_title(jsonObject.getString("original_title"));
                                 modelku.setOverview(jsonObject.getString("overview"));
                                 modelku.setRelease_date(jsonObject.getString("release_date"));
@@ -98,11 +99,19 @@ public class ListData extends AppCompatActivity {
                                 modelku.setVote_count(jsonObject.getInt("vote_count"));
                                 DataArrayList.add(modelku);
                             }
-
+                            //untuk handle click
                             adapter = new dataadapter(DataArrayList, new dataadapter.Callback() {
                                 @Override
                                 public void onClick(int position) {
-
+                                    model movie = DataArrayList.get(position);
+                                    Intent intent = new Intent(getApplicationContext(), DetailMovie.class);
+                                    intent.putExtra("id",movie.id);
+                                    intent.putExtra("judul",movie.original_title);
+                                    intent.putExtra("date",movie.release_date);
+                                    intent.putExtra("deskripsi",movie.overview);
+                                    intent.putExtra("path",movie.poster_path);
+                                    startActivity(intent);
+                                    Toast.makeText(ListData.this, ""+position, Toast.LENGTH_SHORT).show();
                                 }
 
                                 @Override
@@ -116,19 +125,17 @@ public class ListData extends AppCompatActivity {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+
                     }
 
                     @Override
                     public void onError(ANError error) {
-
+                        // handle error
                         Log.d("errorku", "onError errorCode : " + error.getErrorCode());
                         Log.d("errorku", "onError errorBody : " + error.getErrorBody());
                         Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
                     }
                 });
-
     }
+
 }
-
-
-
